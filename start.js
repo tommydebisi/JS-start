6b3e55029512452d644765a3286545c65db9ce3f
tommydebisi <tommydebisi@gmail.com>
2022-06-24 15:37:00 +0100

    :rocket: files added to be deployed

---
+++ start.js
@@ -0,0 +1,11 @@
+let myDetails = {
+    name: "Name: Adebisi Oluwatomiwa",
+    height: "Height: 162cm",
+    country: "Country: Nigeria"
+};
+
+for (let key in myDetails)
+{
+    document.write(myDetails[key]);
+    document.write("<br>")
+}
